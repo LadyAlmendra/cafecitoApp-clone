e80b7fe2f5dae44ecb6fb3836df112c6e6fb4e18
Almendra Romina R <almendraromina1@gmail.com>
2024-02-23 02:12:00 -0300

    Verificación de estados del servidor

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import {RouterProvider} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import Router from './router/Router';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const App = () => {
+  const server = import.meta.env.VITE_MERCADOPAGO_URL;
   const [serverStatus, setServerStatus] = useState('');
 
   const checkServerStatus = async () => {
+    console.log("Haciendo petición al servidor:", server);
     try {
-      const response = await axios.get(import.meta.env.VITA_MERCADOPAGO_URL); 
-      if (response.status === 200) {
+      const response = await axios.get(server);
+      console.log("Respuesta recibida del servidor:", response); 
+
+      if ([200, 204, 304].includes(response.status)) {
         setServerStatus('El servidor está en línea');
       } else {
         setServerStatus('El servidor está en línea pero devuelve un estado diferente a 200');
       }
     } catch (error) {
+      console.log("Error al conectar con el servidor:", error); 
       setServerStatus('Error al conectar con el servidor');
     }
-    console.log(serverStatus)
   };
 
   useEffect(() => {
     checkServerStatus();
   }, []);
+
+  useEffect(() => {
+    console.log("Estado del servidor:", serverStatus); 
+  }, [serverStatus]);
+
   return (
     <>
       <RouterProvider router={Router} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
